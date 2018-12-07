bb9470a444e7abb10c54f6d3a8345a054b6828a5
yannaungoak <yannaungoak@gmail.com>
2018-12-08 02:27:26 +0800

    basic map and charts functionality

---
+++ prototypes/mapbox_gl_js_deprivation_lsoa/src/components/DonorBarChart/DonorBarChart.stories.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+
+import DonorBarChart from './DonorBarChart'
+
+storiesOf('DonorBarChart', module).add('Example 1', () =>
+  <DonorBarChart />
+)
