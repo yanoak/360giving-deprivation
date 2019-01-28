db50b959703afe2b95c0608dafbc5ebbe0b23311
yannaungoak <yannaungoak@gmail.com>
2019-01-28 19:03:46 +0800

    remove old prototype

--- prototypes/mapbox_gl_js_deprivation_lsoa/src/components/DonorBarChart/DonorBarChart.stories.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { storiesOf } from '@storybook/react'
-
-import DonorBarChart from './DonorBarChart'
-
-storiesOf('DonorBarChart', module).add('Example 1', () =>
-  <DonorBarChart />
-)
