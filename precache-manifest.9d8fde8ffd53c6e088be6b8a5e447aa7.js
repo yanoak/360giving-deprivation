7f467bb6305658ec96d8369054660dfaf89fb371
Yan <yan@Yans-MacBook-Pro.local>
2019-07-08 18:23:15 +0800

    Updates

---
+++ precache-manifest.9d8fde8ffd53c6e088be6b8a5e447aa7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/360giving-deprivation/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f3216256ed5496902ac8",
+    "url": "/360giving-deprivation/static/js/runtime~main.f3216256.js"
+  },
+  {
+    "revision": "d72e055680323ee9582a",
+    "url": "/360giving-deprivation/static/js/main.d72e0556.chunk.js"
+  },
+  {
+    "revision": "709b0c4f694a6f3d6d46",
+    "url": "/360giving-deprivation/static/js/1.709b0c4f.chunk.js"
+  },
+  {
+    "revision": "d72e055680323ee9582a",
+    "url": "/360giving-deprivation/static/css/main.34005520.chunk.css"
+  },
+  {
+    "revision": "709b0c4f694a6f3d6d46",
+    "url": "/360giving-deprivation/static/css/1.441248c0.chunk.css"
+  },
+  {
+    "revision": "4863402ef073d4a6c4dcb0df33befacf",
+    "url": "/360giving-deprivation/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/360giving-deprivation/precache-manifest.af9b372f47ec2d3e0211340668c488b9.js"
+  "/360giving-deprivation/precache-manifest.9d8fde8ffd53c6e088be6b8a5e447aa7.js"
 );
 
 workbox.clientsClaim();
